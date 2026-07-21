93077711877baea672e98e524ce61e17d5a31d71
Vahram16 <51964457+Vahram16@users.noreply.github.com>
2026-07-21 21:14:07 +0400

    Expose validated confirmation messages to scanning

--- src/Services/Identity/Identity.Api/Features/Accounts/ConfirmingEmail/V1/ConfirmEmailCommand.cs
+++ src/Services/Identity/Identity.Api/Features/Accounts/ConfirmingEmail/V1/ConfirmEmailCommand.cs
@@ -2,6 +2,6 @@ using Microservices.Application;
 
 namespace Identity.Api.Features.Accounts.ConfirmingEmail.V1;
 
-internal sealed record ConfirmEmailCommand(
+public sealed record ConfirmEmailCommand(
     Guid UserId,
     string Code) : ICommand;
