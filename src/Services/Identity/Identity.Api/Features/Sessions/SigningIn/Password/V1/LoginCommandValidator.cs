771377c1e7812687b252ba8b0eae35ec9ec60933
Vahram16 <51964457+Vahram16@users.noreply.github.com>
2026-07-26 23:04:40 +0400

    Separate sign-in slices

---
+++ src/Services/Identity/Identity.Api/Features/Sessions/SigningIn/Password/V1/LoginCommandValidator.cs
@@ -0,0 +1,17 @@
+using FluentValidation;
+
+namespace Identity.Api.Features.Sessions.SigningIn.Password.V1;
+
+public sealed class LoginCommandValidator : AbstractValidator<LoginCommand>
+{
+    public LoginCommandValidator()
+    {
+        RuleFor(command => command.Email)
+            .NotEmpty()
+            .MaximumLength(254)
+            .EmailAddress();
+        RuleFor(command => command.Password)
+            .NotEmpty()
+            .MaximumLength(128);
+    }
+}
