d40e60af7a26a77393448b839da7ce26a52878a7
Vahram16 <51964457+Vahram16@users.noreply.github.com>
2026-08-02 18:03:31 +0400

    Index pending bus and consumer outbox roles

--- src/Services/ServiceTemplate/ServiceTemplate.Api/Persistence/ServiceTemplateDbContext.cs
+++ src/Services/ServiceTemplate/ServiceTemplate.Api/Persistence/ServiceTemplateDbContext.cs
@@ -1,3 +1,4 @@
+using MassTransit.EntityFrameworkCoreIntegration;
 using Microservices.Messaging;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,5 +11,21 @@ public sealed class ServiceTemplateDbContext(DbContextOptions<ServiceTemplateDbC
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.AddMassTransitOutboxEntities();
+
+        modelBuilder.Entity<OutboxMessage>()
+            .HasIndex(message => new { message.SentTime, message.OutboxId })
+            .HasDatabaseName("IX_OutboxMessage_BusPending_SentTime")
+            .HasFilter("\"OutboxId\" IS NOT NULL");
+
+        modelBuilder.Entity<OutboxMessage>()
+            .HasIndex(message => new
+            {
+                message.SentTime,
+                message.InboxMessageId,
+                message.InboxConsumerId
+            })
+            .HasDatabaseName("IX_OutboxMessage_ConsumerPending_SentTime")
+            .HasFilter(
+                "\"OutboxId\" IS NULL AND \"InboxMessageId\" IS NOT NULL AND \"InboxConsumerId\" IS NOT NULL");
     }
 }
