d4a985d68e3ec1eb5fc8620c17161377b57caa13
Vahram16 <51964457+Vahram16@users.noreply.github.com>
2026-08-04 13:38:23 +0400

    Defer outbox monitoring indexes until justified

--- src/Services/ServiceTemplate/ServiceTemplate.Api/Persistence/ServiceTemplateDbContext.cs
+++ src/Services/ServiceTemplate/ServiceTemplate.Api/Persistence/ServiceTemplateDbContext.cs
@@ -1,4 +1,3 @@
-using MassTransit.EntityFrameworkCoreIntegration;
 using Microservices.Messaging;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,21 +10,5 @@ public sealed class ServiceTemplateDbContext(DbContextOptions<ServiceTemplateDbC
     {
         base.OnModelCreating(modelBuilder);
         modelBuilder.AddMassTransitOutboxEntities();
-
-        modelBuilder.Entity<OutboxMessage>()
-            .HasIndex(message => new { message.SentTime, message.OutboxId })
-            .HasDatabaseName("IX_OutboxMessage_BusPending_SentTime")
-            .HasFilter("\"OutboxId\" IS NOT NULL");
-
-        modelBuilder.Entity<OutboxMessage>()
-            .HasIndex(message => new
-            {
-                message.SentTime,
-                message.InboxMessageId,
-                message.InboxConsumerId
-            })
-            .HasDatabaseName("IX_OutboxMessage_ConsumerPending_SentTime")
-            .HasFilter(
-                "\"OutboxId\" IS NULL AND \"InboxMessageId\" IS NOT NULL AND \"InboxConsumerId\" IS NOT NULL");
     }
 }
