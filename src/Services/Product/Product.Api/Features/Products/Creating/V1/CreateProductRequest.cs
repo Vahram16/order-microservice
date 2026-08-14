08d147c91ffa919d0d25fee0caa14645a173947c
Vahram16 <51964457+Vahram16@users.noreply.github.com>
2026-08-14 17:02:06 +0400

    Require Product create price in JSON

--- src/Services/Product/Product.Api/Features/Products/Creating/V1/CreateProductRequest.cs
+++ src/Services/Product/Product.Api/Features/Products/Creating/V1/CreateProductRequest.cs
@@ -1,8 +1,10 @@
+using System.Text.Json.Serialization;
+
 namespace Product.Api.Features.Products.Creating.V1;
 
 public sealed record CreateProductRequest(
     string Sku,
     string Name,
     string? Description,
-    decimal Price,
+    [property: JsonRequired] decimal Price,
     string CurrencyCode);
